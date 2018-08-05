960f053ba039f8defac643ebc5b67a3c310c1ace
Vibhav Churi <churi.vibhav@hisabsoftware.co.in>
2018-08-05 12:38:48 +0530

    Fixed build by removing unnecessary dependancy

--- src/main/java/co/hisabsoftware/polling/webback/controllers/PollsController.java
+++ src/main/java/co/hisabsoftware/polling/webback/controllers/PollsController.java
@@ -1,6 +1,5 @@
 package co.hisabsoftware.polling.webback.controllers;
 
-import org.junit.experimental.theories.FromDataPoints;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
